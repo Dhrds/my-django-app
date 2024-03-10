097f6832f2d81ea157965c7782ca4d401d22a08b
Dhrds <dhrds1996@gmail.com>
2024-03-10 17:22:37 -0300

    reduzindo codigo

--- inicializador.py
+++ inicializador.py
@@ -1,45 +1,57 @@
 import os
+import logging
 
 
 nome_do_projeto = input("Digite o nome do projeto: ")
+logging.basicConfig(filename=f"{nome_do_projeto}/project.log",
+                    level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
 
 
 def criar_estrutura_diretorios():
-    diretorios = [
-        f"{nome_do_projeto}/nginx",
-        f"{nome_do_projeto}/django_app",
-        f"{nome_do_projeto}/tests",
-        f"{nome_do_projeto}/.github/workflows",
-        f"{nome_do_projeto}/scripts",
-    ]
-    for diretorio in diretorios:
-        os.makedirs(diretorio, exist_ok=True)
+    try:
+        diretorios = [
+            f"{nome_do_projeto}/nginx",
+            f"{nome_do_projeto}/django_app",
+            f"{nome_do_projeto}/tests",
+            f"{nome_do_projeto}/.github/workflows",
+            f"{nome_do_projeto}/scripts",
+        ]
+        for diretorio in diretorios:
+            os.makedirs(diretorio, exist_ok=True)
+            logging.info(f"Diretório {diretorio} criado com sucesso.")
+    except Exception as e:
+        logging.error(f"Erro ao criar diretórios: {e}")
 
 
-def criar_dockerfile():
+def criar_arquivos(caminho, conteudo):
+    try:
+        with open(caminho, "w", encoding="utf-8") as f:
+            f.write(conteudo)
+            logging.info(f"Arquivo {caminho} criado com sucesso.")
+            f.close()
+    except Exception as e:
+        logging.error(f"Erro ao criar arquivo: {e}")
 
-    with open(f"{nome_do_projeto}/Dockerfile", "w", encoding="utf-8") as f:
-        f.write("""
+
+docker = """
 FROM python:3.9
 
 WORKDIR /app
 
-COPY poetry.lock pyproject.toml ./
+# Copia o arquivo requirements.txt para o contêiner
+COPY requirements.txt ./
 
-RUN pip install poetry && \\
-    poetry config virtualenvs.create false && \\
-    poetry install --no-dev
+# Instala as dependências
+RUN pip install --no-cache-dir -r requirements.txt
 
+# Copia o restante dos arquivos para o contêiner
 COPY . .
-CMD ['python', 'manage.py', 'runserver', '0.0.0.0:8000']
-        """)
 
-
-def criar_docker_compose():
-
-    with open(f"{nome_do_projeto}/docker-compose.yml",
-              "w", encoding="utf-8") as f:
-        f.write("""
+# Comando para executar a aplicação
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+        """
+docker_compose = """
 version: '3'
 
 services:
@@ -74,32 +86,56 @@ services:
     depends_on:
       - web
 
-  monitoring_tool:
-    image: monitoring_tool_image
-        """)
+        """
+script = """
+#!/bin/bash
+
+# Verifica se o Python está instalado
+if ! command -v python &> /dev/null; then
+    echo "Python não está instalado. Por favor, instale o Python."
+    exit 1
+fi
+
+# Verifica se o Docker está instalado
+if ! command -v docker &> /dev/null; then
+    echo "Docker não está instalado. Por favor, instale o Docker."
+    exit 1
+fi
+
+# Verifica se o Docker Compose está instalado
+if ! command -v docker-compose &> /dev/null; then
+    echo "Docker Compose não está instalado.instale o Docker Compose."
+    exit 1
+fi
+
+cd ..
+
+# Cria um ambiente virtual (venv) se não existir
+if [ ! -d "venv" ]; then
+    python -m venv venv
+fi
+
+# Ativa o ambiente virtual
+source venv/Scripts/activate
+
+# Instala o Django dentro do ambiente virtual
+pip install django
 
 
-def criar_envs():
+# Cria um novo projeto Django na pasta 'django_app'
+django-admin startproject {nome_do_projeto} .
 
-    ambientes = ['.dev', '.test', '.prod',
-                 '_exemplo.dev', '_exemplo.test', '_exemplo.prod']
-    for ambiente in ambientes:
-        with open(f"{nome_do_projeto}/.env{ambiente}",
-                  "w", encoding="utf-8") as f:
-            f.write("""
-POSTGRES_DB=nome_do_banco_de_dados
-POSTGRES_USER=nome_do_usuario
-POSTGRES_PASSWORD=sua_senha_secreta
-POSTGRES_HOST=db
+# Retorna para o diretório do script
+cd -
 
-DJANGO_SECRET_KEY=sua_chave_secreta
-DJANGO_DEBUG=True
-            """)
+# Aplica as migrações do banco de dados
+python manage.py migrate
 
+# Inicia o servidor Django
+python manage.py runserver 0.0.0.0:8000
 
-def criar_gitignore():
-    with open(f"{nome_do_projeto}/.gitignore", "w", encoding="utf-8") as f:
-        f.write("""
+    """
+gitignore = """
 # Arquivos de ambiente
 .env.dev
 .env.test
@@ -107,12 +143,8 @@ def criar_gitignore():
 
 # Arquivos de log
 *.log
-        """)
-
-
-def criar_readme():
-    with open(f"{nome_do_projeto}/README.md", "w", encoding="utf-8") as f:
-        f.write("""
+        """
+readme = """
 # My Django App
 
 Este é um projeto Django que utiliza Docker Compose para gerenciar um ambiente
@@ -195,13 +227,8 @@ por favor siga as etapas abaixo:
 ## Licença
 
 Este projeto está licenciado sob a [MIT License](LICENSE).
-        """)
-
-
-def criar_ci_cd():
-    with open(f"{nome_do_projeto}/.github/workflows/ci-cd.yml",
-              "w", encoding="utf-8") as f:
-        f.write("""
+        """
+ci_cd = """
 name: CI/CD
 
 on:
@@ -236,65 +263,8 @@ jobs:
       - name: Build Docker image
         run: |
           docker-compose build
-        """)
-
-
-def criar_script_inicializacao():
-    with open(f"{nome_do_projeto}/scripts/setup.sh",
-              "w", encoding="utf-8") as f:
-        f.write(f"""
-#!/bin/bash
-
-# Verifica se o Python está instalado
-if ! command -v python &> /dev/null; then
-    echo "Python não está instalado. Por favor, instale o Python."
-    exit 1
-fi
-
-# Verifica se o Docker está instalado
-if ! command -v docker &> /dev/null; then
-    echo "Docker não está instalado. Por favor, instale o Docker."
-    exit 1
-fi
-
-# Verifica se o Docker Compose está instalado
-if ! command -v docker-compose &> /dev/null; then
-    echo "Docker Compose não está instalado.instale o Docker Compose."
-    exit 1
-fi
-
-cd ..
-
-# Cria um ambiente virtual (venv) se não existir
-if [ ! -d "venv" ]; then
-    python -m venv venv
-fi
-
-# Ativa o ambiente virtual
-source venv/Scripts/activate
-
-# Instala o Django dentro do ambiente virtual
-pip install django
-
-
-# Cria um novo projeto Django na pasta 'django_app'
-django-admin startproject {nome_do_projeto} .
-
-# Retorna para o diretório do script
-cd -
-
-# Aplica as migrações do banco de dados
-python manage.py migrate
-
-# Inicia o servidor Django
-python manage.py runserver 0.0.0.0:8000
-
-    """)
-
-
-def criar_git():
-    with open(f"{nome_do_projeto}/gitconfig.py", "w", encoding="utf-8") as f:
-        f.write("""
+        """
+git = """
 import git
 
 git.config("--global", "user.name", "Seu Nome")
@@ -305,20 +275,39 @@ repo = git.Repo.init(".")
 repo.index.add(".")
 repo.index.commit("Criando estrutura básica do Django e configurando Git")
 
-                """)
+                """
+
+
+def criar_envs():
+
+    ambientes = ['.dev', '.test', '.prod',
+                 '_exemplo.dev', '_exemplo.test', '_exemplo.prod']
+    for ambiente in ambientes:
+        with open(f"{nome_do_projeto}/.env{ambiente}",
+                  "w", encoding="utf-8") as f:
+            f.write("""
+POSTGRES_DB=nome_do_banco_de_dados
+POSTGRES_USER=nome_do_usuario
+POSTGRES_PASSWORD=sua_senha_secreta
+POSTGRES_HOST=db
+
+DJANGO_SECRET_KEY=sua_chave_secreta
+DJANGO_DEBUG=True
+            """)
 
 
 def main():
 
     criar_estrutura_diretorios()
-    criar_dockerfile()
-    criar_docker_compose()
     criar_envs()
-    criar_readme()
-    criar_gitignore()
-    criar_ci_cd()
-    criar_script_inicializacao()
-    criar_git()
+    criar_arquivos(f"{nome_do_projeto}/Dockerfile", docker)
+    criar_arquivos(f"{nome_do_projeto}/docker-compose.yml", docker_compose)
+    criar_arquivos(f"{nome_do_projeto}/scripts/setup.sh", script)
+    criar_arquivos(f"{nome_do_projeto}/.gitignore", gitignore)
+    criar_arquivos(f"{nome_do_projeto}/README.md", readme)
+    criar_arquivos(f"{nome_do_projeto}/requirements.txt", '')
+    criar_arquivos(f"{nome_do_projeto}/.github/workflows/ci-cd.yml", ci_cd)
+    criar_arquivos(f"{nome_do_projeto}/gitconfig.py", git)
     print("Arquivos criados com sucesso!")
 
 
